9be7f5039dc9846ccd8001f88fdc60a7cdad1282
Juhamatti Niemelä <iiska@iki.fi>
2008-12-26 14:11:50 +0200

    Irkov class.

--- irkov.rb
+++ irkov.rb
@@ -4,36 +4,45 @@ require 'markov_chain'
 
 # Parse a few random irc log files from the directories given
 # as command line parameters.
-files = []
-ARGV.each{|arg|
-  if (File.directory?(arg))
-    Dir.new(arg).each {|f|
-        files << arg + '/' + f if File.file?(arg + '/' + f)
+class Irkov
+  def initialize(dirs)
+    files = []
+    dirs.each{|arg|
+      if (File.directory?(arg))
+        Dir.new(arg).each {|f|
+          files << arg + '/' + f if File.file?(arg + '/' + f)
+        }
+      elsif (File.file?(arg))
+        files << arg
+      end
     }
-  elsif (File.file?(arg))
-    files << arg
+
+    selected = []
+    5.times{
+      selected << files.delete_at(rand(files.size))
+    }
+
+    lines = []
+    re = Regexp.new('^\d\d:\d\d +<.+> +(.*)$')
+    selected.each{|f|
+      File.new(f).each{|l|
+        m = re.match(l)
+        lines << m[1] if m
+      }
+    }
+
+    @markov = MarkovChain.new(lines.join(' '))
   end
-}
 
-selected = []
-5.times{
-  selected << files.delete_at(rand(files.size))
-}
+  def say
+    msg = []
+    (4 + rand(10)).times{|c|
+      msg << @markov.next(msg[c-1])
+    }
 
-lines = []
-re = Regexp.new('^\d\d:\d\d +<.+> +(.*)$')
-selected.each{|f|
-  File.new(f).each{|l|
-    m = re.match(l)
-    lines << m[1] if m
-  }
-}
+    return msg.join(' ')
+  end
+end
 
-m = MarkovChain.new(lines.join(' '))
-
-msg = []
-(4 + rand(10)).times{|c|
-  msg << m.next(msg[c-1])
-}
-
-puts msg.join(' ')
+bot = Irkov.new(ARGV)
+puts bot.say
